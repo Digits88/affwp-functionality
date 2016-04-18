28fda4654265cd47a62a4321c8b41d78c0b5168f
Pippin Williamson <pippin@pippinsplugins.com>
2016-04-18 10:37:45 -0500

    Grandfather customers into 40% discount that purchased before April 18, 2016

--- affwp-functionality.php
+++ affwp-functionality.php
@@ -400,3 +400,18 @@ function affwp_feed_query( $query ) {
 	}
 }
 add_action( 'pre_get_posts', 'affwp_feed_query', 99999999 );
+
+/*
+ * Sets renewal discount to 40% for any customer that purchased before April 18, 2016
+ */
+function affwp_grandfather_renewal_discount( $renewal_discount, $license_id ) {
+
+	$license = get_post( $license_id );
+
+	if( strtotime( $license->post_date ) < strtotime( 'April 18, 2016' ) ) {
+		$discount = 40;
+	}
+
+	return $discount;
+}
+add_filter( 'edd_sl_renewal_discount_percentage', 'affwp_grandfather_renewal_discount', 10, 2 );
